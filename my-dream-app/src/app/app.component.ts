e47bdaa3174ea3ea17a2af0969681bb5ad911879
Scott Isitt <isitts@hotmail.com>
2022-06-14 00:54:09 -0700

    Uncomment out title in app.component.ts.

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'The greatest'; 
+  title = 'The greatest'; 
   name = 'Scott';
 }
