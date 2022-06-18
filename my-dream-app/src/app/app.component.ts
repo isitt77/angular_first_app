ab53080d34d40443cd01647cf1e3e532159cdf9d
Scott Isitt <isitts@hotmail.com>
2022-06-17 23:15:03 -0700

    Example of in-line CSS styling in app TS module.

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    p{
+      color: purple;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'The greatest'; 
+  title = 'The greatest';
   name = 'Scott';
 }
