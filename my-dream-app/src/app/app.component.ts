ea1ebedf10d22e4c163171ff6f529cd9f49cd186
Scott Isitt <isitts@hotmail.com>
2022-06-17 23:18:14 -0700

    Revert back to styleUrl in app TS module.

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    p{
-      color: purple;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'The greatest';
