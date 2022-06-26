8f92431fefe52f01fd93b5bc47c572a8be0125f1
Scott Isitt <isitts@hotmail.com>
2022-06-26 15:39:05 -0700

    Create array for servers in servers TS component. Create ngFor directive in servers html component to list our servers.

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   severCreationStatus = 'No server was created.';
   serverName = "";
   serverCreated = false;
+  servers = ['TestServer1', 'TestServerr2'];
 
   constructor() {
     setTimeout(() => {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     if (this.serverName != '') {
+      this.servers.push(this.serverName);
       this.serverCreated = true;
       this.severCreationStatus = `${this.serverName} was created.`;
     }
